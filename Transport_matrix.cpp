eb0c33d99ed81362c25c36b95cdd1fba2ff4cd7c
Mohammad ali mazdarani <mazdarani26.gmail.com>
2022-12-27 04:43:43 -0800

    Complete project

--- Transport_matrix.cpp
+++ Transport_matrix.cpp
@@ -14,6 +14,9 @@ using namespace std;
 //prototype
 
 void showmatrix1(int d[][2],int r,int c);
+void showmatrix2(int d[][3],int r,int c);
+void matrixTransport(int e[][2],int rd,int cd);
+
 //--------------------------
 //micro
 //---------------------
@@ -25,6 +28,7 @@ int main(int argc, char** argv)
 //Declare matrix array
 int a[3][2] = {{1,2},{3,4},{5,6}};
 
+
 cout<<"Matrix a Before Transport :"<<endl<<endl;
 
 showmatrix1(a,3,2);
@@ -32,9 +36,28 @@ showmatrix1(a,3,2);
 //Seperator
 cout<<endl<<"----------------------------------"<<endl;
 
+//Declare matrix array
+int b[2][3] = {0};
+
+
+cout<<"Matrix for result a Transport c:"<<endl<<endl;
+
+showmatrix2(b,2,3);
+
+//Seperator
+cout<<endl<<"----------------------------------"<<endl;
+
+cout<<"Matrix result a Transport c:"<<endl<<endl;
+
+matrixTransport(a,3,2);
+
+//Seperator
+cout<<endl<<"----------------------------------"<<endl;
+
 getch();
 }
 //function variable
+ int k[2][3] = {0};
 //--------------------------
 //functions
 
@@ -52,5 +75,57 @@ for(int i = 0; i<r; ++i)
     cout<<endl;
     
 }
+}
 
+//-------------------------------------
+void showmatrix2(int d[][3],int r,int c)
+{
+
+for(int i = 0; i<r; ++i)
+{
+
+    for(int j = 0; j<c; ++j)
+    {
+        cout<<setw(4)<<d[i][j];
+    }
+    cout<<endl;
+    
+}
+}
+
+//---------------------------
+
+//Function for Transport matrix
+
+void matrixTransport(int e[][2],int rd,int cd)
+
+{
+    
+   
+
+    for(int i=0; i<rd; ++i)
+    {
+        for(int j=0; j<cd; ++j)
+        {
+
+             k[j][i] = e[i][j];
+             cout<<"k["<<j<<"]"<<"["<<i<<"]"<<" = "<<k[j][i]<<endl;
+        }
+
+    }
+
+cout<<endl<<"--------------------------"<<endl;
+
+    //Show transport array
+
+        for(int i=0; i<cd; ++i)
+    {
+        for(int j=0; j<rd; ++j)
+        {
+              cout<<setw(4)<<k[i][j];
+        }
+        cout<<endl;
+        
+    }
+    
 }
